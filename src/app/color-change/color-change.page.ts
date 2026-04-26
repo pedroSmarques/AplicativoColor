72abbe494ac04ebc2696d4ceb5b21b889c9ae3fd
Pedro Marques <pedromarquess2006@gmail.com>
2026-04-26 19:35:42 -0300

    Finalização dos codigos

--- src/app/color-change/color-change.page.ts
+++ src/app/color-change/color-change.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonRange, IonCard, IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-color-change',
@@ -12,9 +13,36 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonRange, IonCard, IonButt
 })
 export class ColorChangePage implements OnInit {
 
-  constructor() { }
+
+
+  constructor(private rota: Router) { }
 
   ngOnInit() {
   }
 
+  navegarParaHome() {
+    this.rota.navigate(['/home']);
+  }
+
+  //PEGANDO OS VALORES DA RANGES
+
+  r: number = 0;
+  g: number = 0;
+  b: number = 0;
+
+  corBotao: string = 'rgb(0,0,0)';
+  corFundo: string = 'linear-gradient(180deg, #0b1f5c, #000000)';
+
+  //BOTÕES PARA ALTERAR COR
+
+  mudarCorBotao() {
+    this.corBotao = `rgb(${this.r}, ${this.g}, ${this.b})`;
+  }
+
+  mudarFundo() {
+    const cor = `rgb(${this.r}, ${this.g}, ${this.b})`;
+    this.corFundo = `linear-gradient(180deg, ${cor}, #000000)`;
+  }
+
 }
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -18,8 +19,10 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, I
     IonIcon]
 })
 export class HomePage {
-  constructor() {}
+  constructor(private rota: Router) {}
 
-    
+    navegarParaColor(){
+      this.rota.navigate(['/color-change']);
+    }
 
 }
